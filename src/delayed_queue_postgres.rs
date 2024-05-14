1fad44145f86133be11993e37e179255aae89ed4
Vlad Platon <platonnvlad@gmail.com>
2024-05-14 19:53:59 +0300

    Add poll method
    Signed-off-by: Vlad Platon <platonnvlad@gmail.com>

--- src/delayed_queue_postgres.rs
+++ src/delayed_queue_postgres.rs
@@ -36,6 +36,50 @@ impl DelayedQueuePostgres {
         messages.into_iter().collect()
     }
 
+    pub async fn poll(self: &Self, kind: &str, now: u64) -> Result<Option<Message>> {
+        Box::pin(async move {
+            let message_opt = self.get_scheduled(kind, now).await?;
+
+            match message_opt {
+                Some(message) => {
+                    let lock_result = self.lock(&message.key, kind, message.scheduled_at).await?;
+
+                    if lock_result {
+                        Ok(Some(message))
+                    } else {
+                        Ok(self.poll(kind, now).await?)
+                    }
+                }
+                None => Ok(None),
+            }
+        })
+        .await
+    }
+
+    pub async fn ack(
+        self: &Self,
+        key: &str,
+        kind: &str,
+        created_at: u64,
+        scheduled_at: u64,
+    ) -> Result<()> {
+        sqlx::query!(
+            r#"
+			DELETE FROM DelayedQueue
+				WHERE
+				pkey = $1 AND pkind = $2 AND created_at = $3 AND scheduled_at = $4
+			"#,
+            key,
+            kind,
+            created_at as i64,
+            scheduled_at as i64
+        )
+        .execute(&self.pool)
+        .await?;
+
+        Ok(())
+    }
+
     pub async fn schedule_message(self: &Self, message: &Message) -> Result<()> {
         sqlx::query!(
             r#"
@@ -54,5 +98,54 @@ impl DelayedQueuePostgres {
         .await?;
 
         Ok(())
+    }
+
+    async fn get_scheduled(self: &Self, kind: &str, now: u64) -> Result<Option<Message>> {
+        let result = sqlx::query!(
+            r#"
+			SELECT pkey, payload, scheduled_at, created_at
+				FROM DelayedQueue
+				WHERE
+				pkind = $1 AND scheduled_at <= $2
+				ORDER BY scheduled_at
+			"#,
+            kind,
+            now as i64
+        )
+        .map(|row| Message {
+            key: row.pkey.clone(),
+            kind: kind.to_string(),
+            payload: row.payload.clone(),
+            scheduled_at: row.scheduled_at as u64,
+            scheduled_at_initially: row.scheduled_at as u64,
+            created_at: row.created_at as u64,
+        })
+        .fetch_optional(&self.pool)
+        .await?;
+
+        Ok(result)
+    }
+
+    async fn lock(self: &Self, key: &str, kind: &str, scheduled_at: u64) -> Result<bool> {
+        let result = sqlx::query!(
+            r#"
+			UPDATE DelayedQueue
+			SET
+			scheduled_at = $4
+			WHERE
+			pkey = $1 AND
+			pkind = $2 AND
+			scheduled_at = $3
+			;
+			"#,
+            key,
+            kind,
+            scheduled_at as i64,
+            (scheduled_at as i64) + 60 * 5
+        )
+        .execute(&self.pool)
+        .await?;
+
+        Ok(result.rows_affected() == 1)
     }
 }
